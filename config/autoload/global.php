8e75dc956206f435666d2d5559de31e08fac2431
Andrej Sinicyn <sinicyn@innosoft.de>
2017-04-06 16:42:05 +0200

    - Progress on sessions and cache.

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * @link      https://github.com/rarog/TranslationManagerForAndroidApps for the canonical source repository
+ * @copyright Copyright (c) 2017 Andrej Sinicyn
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
+ */
+/**
  * Global Configuration Override
  *
  * You can use this file for overriding configuration values from modules, etc.
@@ -12,5 +17,36 @@
  */
 
 return [
-    // ...
+    'caches' => [
+        'Cache\Persistent' => [
+            'adapter' => 'filesystem',
+            'options' => [
+                'cache_dir' => __DIR__ . '/../../data/cache/',
+                'ttl' => 86400,
+                'namespace' => 'tmfaa:cache',
+            ],
+            'plugins' => [
+                'serializer',
+            ],
+        ],
+    ],
+    'session' => [
+        'config' => [
+            'class' => 'Zend\Session\Config\SessionConfig',
+            'options' => [
+                'name' => 'ordermanager:session',
+                'gc_maxlifetime' => 900,
+                'remember_me_seconds' => 900,
+            ],
+        ],
+        'storage' => 'Zend\Session\Storage\SessionArrayStorage',
+        'validators' => [
+            'Zend\Session\Validator\RemoteAddr',
+            'Zend\Session\Validator\HttpUserAgent',
+        ],
+    ],
+    'settings' => [
+        'translator_cache' => 'Cache\Persistent',
+        'session_cache' => 'Cache\Persistent',
+    ],
 ];
--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -11,8 +11,90 @@ class Module
 {
     const VERSION = '0.1-dev';
 
+    public function bootstrapSession($e)
+    {
+        $session = $e->getApplication()
+                     ->getServiceManager()
+                     ->get('Zend\Session\SessionManager');
+
+        try {
+            $session->start();
+        } catch (\Exception $e) {
+            session_unset();
+            $session->start();
+        }
+
+        $container = new Container('initialized');
+        if (!isset($container->init)) {
+            $serviceManager = $e->getApplication()
+                                ->getServiceManager();
+            $request = $serviceManager->get('Request');
+
+            $session->regenerateId(true);
+            $container->init          = 1;
+            $container->remoteAddr    = $request->getServer()->get('REMOTE_ADDR');
+            $container->httpUserAgent = $request->getServer()->get('HTTP_USER_AGENT');
+
+            $config = $serviceManager->get('Config');
+            if (!isset($config['session'])) {
+                return;
+            }
+
+            $sessionConfig = $config['session'];
+            if (isset($sessionConfig['validators'])) {
+                $chain = $session->getValidatorChain();
+
+                foreach ($sessionConfig['validators'] as $validator) {
+                    switch ($validator) {
+                        case 'Zend\Session\Validator\HttpUserAgent':
+                            $validator = new $validator($container->httpUserAgent);
+                            break;
+                        case 'Zend\Session\Validator\RemoteAddr':
+                            $validator  = new $validator($container->remoteAddr);
+                            break;
+                        default:
+                            $validator = new $validator();
+                    }
+
+                    $chain->attach('session.validate', array($validator, 'isValid'));
+                }
+            }
+        }
+    }
+
+    public function bootstrapTranslator($e)
+    {
+        $serviceManager = $e->getApplication()
+                            ->getServiceManager();
+
+        $translatorCache = null;
+        $config = $serviceManager->get('Config');
+        if (isset($config) &&
+            isset($config['settings']) &&
+            isset($config['settings']['translator_cache']) &&
+            !empty($cacheName = trim($config['settings']['translator_cache'])) &&
+            $serviceManager->has($cacheName)) {
+            try {
+                $translatorCache = $serviceManager->get($cacheName);
+            } catch (\Exception $e) {
+                $translatorCache = null;
+            }
+        }
+
+        $translator = $serviceManager->get('translator');
+        if (!is_null($translatorCache)) {
+            $translator->setCache($translatorCache);
+        }
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        $this->bootstrapSession($e);
+        $this->bootstrapTranslator($e);
     }
 }
