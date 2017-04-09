cc26328273c299a711af45fc71b18c804e382f1a
rarog <rarogit@gmail.com>
2017-04-09 16:03:44 +0200

    - Implementing dynamic language switch in setup module.

--- module/Setup/src/Setup/Controller/SetupController.php
+++ module/Setup/src/Setup/Controller/SetupController.php
@@ -13,29 +13,66 @@ use Zend\View\Model\ViewModel;
 class SetupController extends AbstractActionController
 {
     protected $translator;
+    protected $container;
+    protected $setupConfig;
+    protected $availableLanguages;
+    protected $fallbackLocale;
+
+    protected function getSetupConfig()
+    {
+        if (is_null($this->setupConfig)) {
+            $this->setupConfig = $this->configHelp('setup');
+        }
+        return $this->setupConfig;
+    }
+
+
+    protected function getAvailableLanguages()
+    {
+        if (is_null($this->availableLanguages)) {
+            $this->availableLanguages = $this->getSetupConfig()->available_languages->toArray();
+        }
+        return $this->availableLanguages;
+    }
+
+    protected function setCurrentLanguage()
+    {
+        if (!is_null($this->container->currentLanguage) &&
+            array_key_exists($this->container->currentLanguage, $this->getAvailableLanguages())) {
+            $this->translator
+                 ->setFallbackLocale($this->fallbackLocale)
+                 ->setLocale($this->container->currentLanguage);
+        }
+    }
 
     public function __construct(\Zend\Mvc\I18n\Translator $translator)
     {
         $this->translator = $translator;
+        $this->container = new \Zend\Session\Container('setup');
+        $this->fallbackLocale = $translator->getLocale();
     }
 
     public function indexAction()
     {
-        $setupConfig = $this->configHelp('setup');
-        $languages = $setupConfig->available_languages->toArray();
+        $this->setCurrentLanguage();
         
         $step1 = new \Setup\Model\Step1(array(
             'setup_language' => $this->translator->getLocale(),
         ));
 
         $formStep1 = new \Setup\Form\Step1Form();
-        $formStep1->get('setup_language')->setValueOptions($languages);
+        $formStep1->get('setup_language')->setValueOptions($this->getAvailableLanguages());
         $formStep1->bind($step1);
 
         $request  = $this->getRequest();
         if ($request->isPost()) {
             $formStep1->setInputFilter($step1->getInputFilter());
             $formStep1->setData($request->getPost());
+             if ($formStep1->isValid() &&
+                 array_key_exists($step1->SetupLanguage, $this->getAvailableLanguages())) {
+                 $this->container->currentLanguage = $step1->SetupLanguage;
+                 $this->setCurrentLanguage();
+             }
         }
 
         return new ViewModel(array(
