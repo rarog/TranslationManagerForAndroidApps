08b3099d8f958a75c31e7cc1d379bca118cad133
rarog <rarogit@gmail.com>
2017-04-27 09:03:58 +0200

    - Adding UserRbac table to schema.

--- data/schema/schema.mysql.sql
+++ data/schema/schema.mysql.sql
@@ -1,9 +1,16 @@
-CREATE TABLE `user`
-(
-    `user_id`       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `username`      VARCHAR(255) DEFAULT NULL UNIQUE,
-    `email`         VARCHAR(255) DEFAULT NULL UNIQUE,
-    `display_name`  VARCHAR(50) DEFAULT NULL,
-    `password`      VARCHAR(128) NOT NULL,
-    `state`         SMALLINT UNSIGNED
-) ENGINE=InnoDB CHARSET="utf8";
+CREATE TABLE `user` (
+  `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(255) DEFAULT NULL UNIQUE,
+  `email` varchar(255) DEFAULT NULL UNIQUE,
+  `display_name` varchar(50) DEFAULT NULL,
+  `password` varchar(128) NOT NULL,
+  `state` smallint(5) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_role_linker` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `role_id` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `user_role_linker_fk1_idx` (`user_id`),
+  CONSTRAINT `user_role_linker_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
