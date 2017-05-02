08e0b4190eb2d4a1960762c148822f6df190f182
rarog <rarogit@gmail.com>
2017-05-02 11:06:45 +0200

    - Adding missing SQLite table schema.

--- data/schema/schema.sqlite.sql
+++ data/schema/schema.sqlite.sql
@@ -7,3 +7,11 @@ CREATE TABLE user
     password      VARCHAR(128) NOT NULL,
     state         SMALLINT
 );
+
+CREATE TABLE user_role_linker
+(
+    user_id  INTEGER NOT NULL,
+    role_id  VARCHAR(55) NOT NULL,
+    PRIMARY KEY(user_id,role_id),
+    FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
