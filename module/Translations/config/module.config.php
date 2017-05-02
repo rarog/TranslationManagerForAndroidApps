6212e0f7d2d0cfc9c7dcf55703464674f092292a
Andrej Sinicyn <rarogit@gmail.com>
2017-05-03 00:59:35 +0200

    - Adding translation support to Translations module.

--- module/Translations/config/module.config.php
+++ module/Translations/config/module.config.php
@@ -28,6 +28,15 @@ return [
             ],
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ],
+        ],
+    ],
     'view_manager' => [
         'template_path_stack' => [
             'translations' => __DIR__ . '/../view',
