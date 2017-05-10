07761d0812ba9f7d8e96515142b110c5313e89b3
rarog <rarogit@gmail.com>
2017-05-10 19:47:20 +0200

    - Adding default ordering by name.

--- module/Translations/src/Model/TeamTable.php
+++ module/Translations/src/Model/TeamTable.php
@@ -8,6 +8,7 @@
 namespace Translations\Model;
 
 use RuntimeException;
+use Zend\Db\Sql\Select;
 use Zend\Db\TableGateway\TableGateway;
 
 class TeamTable
@@ -35,7 +36,13 @@ class TeamTable
      */
     public function fetchAll($where = null)
     {
-        return $this->tableGateway->select($where);
+        return $this->tableGateway->select(
+            function (Select $select) use ($where){
+                if ($where) {
+                    $select->where($where);
+                }
+                $select->order(['name ASC']);
+            });
     }
 
     /**
