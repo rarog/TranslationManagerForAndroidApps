4bcf3fbbba7408ffda434471a4807b57eb414ad2
rarog <rarogit@gmail.com>
2017-05-10 22:32:39 +0200

    - Changing constraint of team_id for apps.

--- data/schema/schema.mysql.sql
+++ data/schema/schema.mysql.sql
@@ -30,9 +30,9 @@ CREATE TABLE `user_settings` (
 
 CREATE TABLE `app` (
     `id`                 INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `team_id`            INT(11) UNSIGNED NOT NULL,
+    `team_id`            INT(11) UNSIGNED NULL,
     `name`               VARCHAR(255) DEFAULT NULL,
     `git_repository`     VARCHAR(4096) DEFAULT NULL,
     `path_to_res_folder` VARCHAR(4096) DEFAULT NULL,
-    CONSTRAINT `app_fk1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT `app_fk1` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- data/schema/schema.sqlite.sql
+++ data/schema/schema.sqlite.sql
@@ -31,9 +31,9 @@ CREATE TABLE user_settings (
 
 CREATE TABLE app (
     id                 INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    team_id            INTEGER NOT NULL,
+    team_id            INTEGER NULL,
     name               VARCHAR(255) DEFAULT NULL,
     git_repository     VARCHAR(4096) DEFAULT NULL,
     path_to_res_folder VARCHAR(4096) DEFAULT NULL,
-    FOREIGN KEY(team_id) REFERENCES team(id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY(team_id) REFERENCES team(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
