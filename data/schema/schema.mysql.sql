8cfe197e3e41e90f32c7e07b0b0154b0a92db3d9
rarog <rarogit@gmail.com>
2017-05-10 22:37:48 +0200

    - Refactoring SQL.

--- data/schema/schema.mysql.sql
+++ data/schema/schema.mysql.sql
@@ -23,14 +23,14 @@ CREATE TABLE `team` (
 CREATE TABLE `user_settings` (
     `user_id` INT(11) UNSIGNED NOT NULL PRIMARY KEY,
     `locale`  VARCHAR(5) NOT NULL,
-    `team_id` INT(11) UNSIGNED NULL,
+    `team_id` INT(11) UNSIGNED DEFAULT NULL,
     CONSTRAINT `user_settings_fk1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `user_settings_fk2` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `app` (
     `id`                 INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `team_id`            INT(11) UNSIGNED NULL,
+    `team_id`            INT(11) UNSIGNED DEFAULT NULL,
     `name`               VARCHAR(255) DEFAULT NULL,
     `git_repository`     VARCHAR(4096) DEFAULT NULL,
     `path_to_res_folder` VARCHAR(4096) DEFAULT NULL,
--- data/schema/schema.sqlite.sql
+++ data/schema/schema.sqlite.sql
@@ -5,7 +5,7 @@ CREATE TABLE user
     email        VARCHAR(255) DEFAULT NULL UNIQUE,
     display_name VARCHAR(50) DEFAULT NULL,
     password     VARCHAR(128) NOT NULL,
-    state        SMALLINT
+    state        SMALLINT DEFAULT NULL
 );
 
 CREATE TABLE user_role_linker
@@ -24,14 +24,14 @@ CREATE TABLE team (
 CREATE TABLE user_settings (
     user_id INTEGER NOT NULL PRIMARY KEY,
     locale  VARCHAR(5) NOT NULL,
-    team_id INTEGER NULL,
+    team_id INTEGER DEFAULT NULL,
     FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (team_id) REFERENCES team(id) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE app (
     id                 INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    team_id            INTEGER NULL,
+    team_id            INTEGER DEFAULT NULL,
     name               VARCHAR(255) DEFAULT NULL,
     git_repository     VARCHAR(4096) DEFAULT NULL,
     path_to_res_folder VARCHAR(4096) DEFAULT NULL,
