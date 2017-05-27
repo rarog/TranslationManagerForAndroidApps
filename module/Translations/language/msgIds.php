31f7ad16a8550f4555b710e0f6c3b361a5086d73
rarog <rarogit@gmail.com>
2017-05-27 12:22:24 +0200

    - Comments

--- module/Translations/language/msgIds.php
+++ module/Translations/language/msgIds.php
@@ -3,5 +3,9 @@
  * Ids for poedit
  * @method translate()
  */
+
+// Translations\Validator\ResFileName
 translate('Resource file name must end with ".xml"');
+
+// Translations\Validator\ResValuesName
 translate('Resource values folder name must be equal to "values" or begin with "values-"');
