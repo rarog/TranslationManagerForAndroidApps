52303b29cfb9efdcd0609809fd14205eaff8fc3a
rarog <rarogit@gmail.com>
2017-07-30 11:26:54 +0200

    - Implementing most of the selection logic.

--- public/js/translations.js
+++ public/js/translations.js
@@ -3,16 +3,65 @@
  * @copyright Copyright (c) 2017 Andrej Sinicyn
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
  */
+function hideSelectionHint(hide) {
+    var node = $("#selectionHint");
+    if (hide) {
+        node.addClass("hidden");
+    } else {
+        node.removeClass("hidden");
+    }
+}
+
+function hideSpinner(hide) {
+    var node = $("#spinner");
+    if (hide) {
+        node.addClass("hidden");
+    } else {
+        node.removeClass("hidden");
+    }
+}
+
+function hideTranslationRow(hide) {
+    var node = $("#translationRow");
+    if (hide) {
+        node.addClass("hidden");
+    } else {
+        node.removeClass("hidden");
+    }
+}
+
+function setSelectionNeededState() {
+    hideSelectionHint(false);
+    hideSpinner(true);
+    hideTranslationRow(true);
+}
+
 $("#app").on("changed.bs.select", function(event, clickedIndex, newValue, oldValue) {
+    setSelectionNeededState();
+
     var resourceSelect = $("#resource");
     $("option", resourceSelect).remove();
     if (newValue) {
-    	var key = $(this).val();
-    	if (key in resources) {
-    		$.each(resources[key], function(index, value) {
-    			// TODO: add options to resourceSelect
-    		});
-    	}
+        var key = $(this).val();
+        if (key in resources) {
+            $.each(resources[key], function(index, value) {
+                resourceSelect.append('<option value="' + index + '">' + value + '</option>');
+            });
+        }
     }
     resourceSelect.selectpicker("refresh");
+});
+
+$("#resource").on("changed.bs.select", function(event, clickedIndex, newValue, oldValue) {
+    if (newValue) {
+        hideSelectionHint(true);
+        hideSpinner(false);
+        hideTranslationRow(true);
+
+        var app = $("#app").val();
+        var resource = $(this).val();
+        // TODO: Load translations
+    } else {
+        setSelectionNeededState();
+    }
 });
