cbda352ef1d1e51bc8ebcfdded6fb27894214a38
rarog <rarogit@gmail.com>
2017-08-13 17:43:48 +0200

    - Minify the style.

--- minify.sh
+++ minify.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+yui-compressor --type css -o ./public/css/style.min.css ./public/css/style.css
 yui-compressor --type js -o ./public/js/translations.min.js ./public/js/translations.js
 
