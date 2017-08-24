1e98de94b7b22a2a5a85296593d93ed3136ef750
rarog <rarogit@gmail.com>
2017-08-24 16:16:50 +0200

    Android string decoding
    - Work around most common errors from AOSP translations. Especially in
    localisations the strings can be quite malformed.

--- module/Translations/src/Controller/Plugin/DecodeAndroidTranslationString.php
+++ module/Translations/src/Controller/Plugin/DecodeAndroidTranslationString.php
@@ -38,10 +38,37 @@ class DecodeAndroidTranslationString extends AbstractPlugin
     {
         $translationString = (string) $translationString;
 
-        if (mb_substr($translationString, 0, 1) === '"') {
-            $jsonTranslationString = $translationString;
-        } else {
-            $jsonTranslationString = '"' . str_replace('\\\'', '\'', $translationString) . '"';
+        if (($translationString == '') || $translationString == '""') {
+            return '';
+        }
+
+        // TODO: Add following test cases:
+        // - String encapsulated by "
+        // - String not encapsulated by "
+        // - String not beginning with " but ending with " (values/strings.xml -> reset_network_desc)
+        // - Multiline strings with real (non-escaped) newlines (values/strings.xml -> font_size_preview_text_body)
+
+        // Fixing strings stored in multiline format. Why, is it relevant to copypaste Android strings like "font_size_preview_text_body" this way?
+        // 1) Be paranoid about strings form files with Windows newlines
+        $translationString = str_replace("\r\n", "\n", $translationString);
+        // 2) Be paranoid about strings form files with Mac newlines
+        $translationString = str_replace("\r", "\n", $translationString);
+        // 3) Remove newlines and empty spaces before actual text in lines
+        $splitString = explode("\n", $translationString);
+        if (($splitString !== false) && !empty($splitString)) {
+            $translationString = '';
+            foreach ($splitString as $line) {
+                $translationString .= ltrim($line);
+            }
+        }
+
+        $jsonTranslationString = str_replace('\\\'', '\'', $translationString);
+        if (mb_substr($jsonTranslationString, 0, 1) !== '"') {
+            $jsonTranslationString = '"' . $jsonTranslationString;
+        }
+
+        if (mb_strlen(mb_substr($jsonTranslationString, -1, 1) !== '"') || (mb_substr($jsonTranslationString, -1, 2) == '\"')) {
+            $jsonTranslationString .= '"';
         }
 
         try {
