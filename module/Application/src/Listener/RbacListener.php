51404d576fe3ee6af4c2f719de84cbe40accebc0
rarog <rarogit@gmail.com>
2017-08-29 18:42:54 +0200

    - Exchange headers to please PHP CodeSniffer.

--- module/Application/src/Listener/RbacListener.php
+++ module/Application/src/Listener/RbacListener.php
@@ -1,8 +1,15 @@
 <?php
 /**
- * @link      https://github.com/rarog/TranslationManagerForAndroidApps for the canonical source repository
- * @copyright Copyright (c) 2017 Andrej Sinicyn
+ * Translation Manager for Android Apps
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   TranslationManagerForAndroidApps
+ * @author    Andrej Sinicyn <rarogit@gmail.com>
+ * @copyright 2017 Andrej Sinicyn
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
+ * @link      https://github.com/rarog/TranslationManagerForAndroidApps
  */
 
 namespace Application\Listener;
