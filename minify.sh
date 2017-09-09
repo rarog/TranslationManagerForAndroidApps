f1aca5c68a4d6e3f1fe1c43426a307220b884ada
rarog <rarogit@gmail.com>
2017-09-09 22:52:32 +0200

    Port minify script to composer.

--- minify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-yui-compressor --type css -o ./public/css/style.min.css ./public/css/style.css
-yui-compressor --type js -o ./public/js/fixJumpingNavbar.min.js ./public/js/fixJumpingNavbar.js
-yui-compressor --type js -o ./public/js/translations.min.js ./public/js/translations.js
-
