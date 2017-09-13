2693cd57dfd24ccdbe32af52210d58a2cb042476
rarog <rarogit@gmail.com>
2017-09-13 06:56:51 +0200

    Issue #8
    - Add models for suggestion vote

---
+++ module/Translations/src/Model/SuggestionVote.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * Translation Manager for Android Apps
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   TranslationManagerForAndroidApps
+ * @author    Andrej Sinicyn <rarogit@gmail.com>
+ * @copyright 2017 Andrej Sinicyn
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
+ * @link      https://github.com/rarog/TranslationManagerForAndroidApps
+ */
+
+namespace Translations\Model;
+
+use DomainException;
+use Zend\Filter\ToInt;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+use Zend\Stdlib\ArraySerializableInterface;
+
+class SuggestionVote implements ArraySerializableInterface, InputFilterAwareInterface
+{
+    /**
+     * @var null|int
+     */
+    private $suggestionId;
+
+    /**
+     * @var null|int
+     */
+    private $userId;
+
+    /**
+     * @var InputFilter
+     */
+    private $inputFilter;
+
+    /**
+     * Constructor
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = null)
+    {
+        if ($data) {
+            $this->exchangeArray($data);
+        }
+    }
+
+    /**
+     * @param mixed $name
+     * @throws \Exception
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $method = 'get' . $name;
+        if (!method_exists($this, $method)) {
+            throw new \Exception('Invalid property');
+        }
+        return $this->$method();
+    }
+
+    /**
+     * @param mixed $name
+     * @param mixed $value
+     * @throws \Exception
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set' . $name;
+        if (!method_exists($this, $method)) {
+            throw new \Exception('Invalid property');
+        }
+        $this->$method($value);
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getSuggestionId() {
+        return $this->suggestionId;
+    }
+
+    /**
+     * @param null|int $suggestionId
+     */
+    public function setSuggestionId($suggestionId) {
+        if (! is_null($suggestionId)) {
+            $suggestionId = (int) $suggestionId;
+        }
+        $this->suggestionId = $suggestionId;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getUserId() {
+        return $this->userId;
+    }
+
+    /**
+     * @param null|int $userId
+     */
+    public function setUserId($userId) {
+        if (!is_null($userId)) {
+            $userId = (int) $userId;
+        }
+        $this->userId = $userId;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Zend\InputFilter\InputFilterAwareInterface::setInputFilter()
+     */
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(sprintf(
+            '%s does not allow injection of an alternate input filter',
+            __CLASS__
+        ));
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Zend\InputFilter\InputFilterAwareInterface::getInputFilter()
+     */
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add([
+            'name'     => 'suggestion_id',
+            'required' => true,
+            'filters'  => [
+                ['name' => ToInt::class],
+            ],
+        ]);
+        $inputFilter->add([
+            'name'     => 'user_id',
+            'required' => true,
+            'filters'  => [
+                ['name' => ToInt::class],
+            ],
+        ]);
+
+        $this->inputFilter = $inputFilter;
+        return $this->inputFilter;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Zend\Stdlib\ArraySerializableInterface::exchangeArray()
+     */
+    public function exchangeArray(array $array)
+    {
+        $this->SuggestionId = !empty($array['suggestion_id']) ? $array['suggestion_id'] : null;
+        $this->UserId = !empty($array['user_id']) ? $array['user_id'] : null;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Zend\Stdlib\ArraySerializableInterface::getArrayCopy()
+     */
+    public function getArrayCopy()
+    {
+        return [
+            'suggestion_id' => $this->SuggestionId,
+            'user_id' => $this->UserId,
+        ];
+    }
+}
---
+++ module/Translations/src/Model/SuggestionVoteTable.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Translation Manager for Android Apps
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   TranslationManagerForAndroidApps
+ * @author    Andrej Sinicyn <rarogit@gmail.com>
+ * @copyright 2017 Andrej Sinicyn
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
+ * @link      https://github.com/rarog/TranslationManagerForAndroidApps
+ */
+
+namespace Translations\Model;
+
+use RuntimeException;
+use Zend\Db\TableGateway\TableGateway;
+
+class SuggestionVoteTable
+{
+    /**
+     * @var TableGateway
+     */
+    private $tableGateway;
+
+    /**
+     * Constructor
+     *
+     * @param TableGateway $tableGateway
+     */
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    /**
+     * Gets all entries
+     *
+     * @param \Zend\Db\Sql\Where|\Closure|string|array $where
+     * @return \Zend\Db\ResultSet\ResultSet
+     */
+    public function fetchAll($where = null)
+    {
+        return $this->tableGateway->select($where);
+    }
+
+    /**
+     * Get all entries of user
+     *
+     * @param  int $userId
+     * @return \Zend\Db\ResultSet\ResultSet
+     */
+    public function fetchAllOfUser(int $userId)
+    {
+        return $this->fetchAll(['user_id' => $userId]);
+    }
+
+    /**
+     * Get entry
+     *
+     * @param int $suggestionId
+     * @param int $userId
+     * @throws RuntimeException
+     * @return SuggestionVote
+     */
+    public function getSuggestionVote(int $suggestionId, int $userId)
+    {
+        $rowset = $this->tableGateway->select(['suggestion_id' => $suggestionId]);
+        $row = $rowset->current();
+        if (! $row) {
+            throw new RuntimeException(sprintf('Could not find row with identifiers %d,%d', $suggestionId, $userId));
+        }
+
+        return $row;
+    }
+
+    /**
+     * Suggestion vote save function
+     *
+     * @param SuggestionVote $userLanguage
+     * @throws RuntimeException
+     * @return SuggestionVote
+     */
+    public function saveSuggestionVote(SuggestionVote $suggestionVote)
+    {
+        $data = [
+            'suggestion_id' => $suggestionVote->SuggestionId,
+            'user_id' => $suggestionVote->UserId,
+        ];
+
+        $suggestionId = (int) $suggestionVote->SuggestionId;
+        $userId = (int) $suggestionVote->UserId;
+
+        if ($suggestionId === 0 || $userId === 0) {
+            throw new RuntimeException('Cannot handle row with invalid id');
+        }
+
+        try {
+            if (($suggestionVote = $this->getSuggestionVote($suggestionId, $userId))) {
+                return $suggestionVote;
+            }
+        } catch (RuntimeException $e) {
+            $this->tableGateway->insert($data);
+            return $suggestionVote;
+        }
+    }
+
+
+    /**
+     * Suggestion vote delete function
+     *
+     * @param int $suggestionId
+     * @param int $userId
+     */
+    public function deleteUserLanguage(int $suggestionId, int $userId = 0)
+    {
+        $deleteParams = ['suggestion_id' => $suggestionId];
+
+        if ($userId > 0) {
+            $deleteParams = ['user_id' => $userId];
+        }
+
+        $this->tableGateway->delete($deleteParams);
+    }
+}
