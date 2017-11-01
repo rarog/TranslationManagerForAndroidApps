03c4af651995af0c603162844931311a79581221
rarog <rarogit@gmail.com>
2017-11-01 12:52:50 +0100

    Adding missing module file.

---
+++ module/Common/src/Module.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Translation Manager for Android Apps
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   TranslationManagerForAndroidApps
+ * @author    Andrej Sinicyn <rarogit@gmail.com>
+ * @copyright 2017 Andrej Sinicyn
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
+ * @link      https://github.com/rarog/TranslationManagerForAndroidApps
+ */
+
+namespace Common;
+
+class Module
+{
+}
