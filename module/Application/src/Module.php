7e5ea9a3d0fa849455b0c0f87de2f3654c6b9df7
rarog <rarogit@gmail.com>
2018-04-07 21:23:58 +0200

    Releasing 0.4

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -31,7 +31,7 @@ use Zend\Session\Config\SessionConfig;
 
 class Module implements BootstrapListenerInterface, ConfigProviderInterface, ServiceProviderInterface
 {
-    const VERSION = '0.4-dev';
+    const VERSION = '0.4';
 
     /**
      * @var Container
