1cf8a3ad317c3d573384af636617f914e12b02d7
rarog <rarogit@gmail.com>
2018-05-01 18:54:29 +0200

    Refactoring IndexControllerTest to use AbstractHttpControllerTestCase.

--- module/Application/test/Controller/IndexControllerTest.php
+++ module/Application/test/Controller/IndexControllerTest.php
@@ -11,50 +11,112 @@
  * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License version 3 or later
  * @link      https://github.com/rarog/TranslationManagerForAndroidApps
  */
+
 namespace ApplicationTest\Controller;
 
+use Application\Module;
 use Application\Controller\IndexController;
-use PHPUnit\Framework\TestCase;
-use Zend\Http\Request;
-use Zend\Mvc\MvcEvent;
-use Zend\Router\RouteMatch;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+use ZfcRbac\Guard\RoutePermissionsGuard;
+use ZfcRbac\Service\AuthorizationService;
+use ReflectionClass;
 
-class IndexControllerTest extends TestCase
+class IndexControllerTest extends AbstractHttpControllerTestCase
 {
+    protected $authorizationService;
 
     public function setUp()
     {
-        $this->controller = new IndexController();
-        $this->request = new Request();
-        $this->routeMatch = new RouteMatch([
-            'controller' => 'application'
-        ]);
-        $this->event = new MvcEvent();
-        $this->event->setRouteMatch($this->routeMatch);
-        $this->controller->setEvent($this->event);
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../../config/application.config.php',
+            $configOverrides
+        ));
+
+        parent::setUp();
+
+        // Tricking: on console requests application doesn't run through most onBootstrap events,
+        // thus not trying to instantiate session and other things not needed for testing
+        $this->setUseConsoleRequest(true);
+        $serviceManager = $this->getApplicationServiceLocator();
+        $this->setUseConsoleRequest(false);
+
+        $authorizationService = $this->prophesize(AuthorizationService::class);
+        $this->authorizationService = $authorizationService;
+
+        $guards = $serviceManager->get('ZfcRbac\Guards');
+        foreach ($guards as $guard) {
+            if ($guard instanceof RoutePermissionsGuard) {
+                $reflection = new ReflectionClass(RoutePermissionsGuard::class);
+                $authorizationServiceProperty = $reflection->getProperty('authorizationService');
+                $authorizationServiceProperty->setAccessible(true);
+                $authorizationServiceProperty->setValue($guard, $authorizationService->reveal());
+            }
+        }
     }
 
-    public function testIndexActionCanBeAccessed()
+    public function testAboutActionCantBeAccessedByNonUsers()
     {
-        $this->routeMatch->setParam('action', 'index');
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->authorizationService->isGranted('userBase')->willReturn(false);
+
+        $this->dispatch('/application/about', 'GET');
+        $this->assertResponseStatusCode(500);
+        $this->assertModuleName('application');
+        $this->assertControllerName(IndexController::class);
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('application/about');
     }
 
-    public function testAboutActionCanBeAccessed()
+    public function testAboutActionCanBeAccessedByUsers()
     {
-        $this->routeMatch->setParam('action', 'about');
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
+        $this->authorizationService->isGranted('userBase')->willReturn(true);
+
+        $this->dispatch('/application/about', 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('application');
+        $this->assertControllerName(IndexController::class);
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('application/about');
+
+        // ViewModel template containing application title an version
+        $this->assertQueryContentContains(
+            '.container .row .col-md-12 h1',
+            sprintf('Translation Manager for Android Apps v%s', Module::VERSION)
+        );
+    }
+
+    public function testIndexActionCantBeAccessedByNonUsers()
+    {
+        $this->authorizationService->isGranted('userBase')->willReturn(false);
+
+        $this->dispatch('/', 'GET');
+        $this->assertResponseStatusCode(500);
+        $this->assertModuleName('application');
+        $this->assertControllerName(IndexController::class);
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('home');
+    }
+
+    public function testIndexActionCanBeAccessedByUsers()
+    {
+        $this->authorizationService->isGranted('userBase')->willReturn(true);
+
+        $this->dispatch('/', 'GET');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('application');
+        $this->assertControllerName(IndexController::class);
+        $this->assertControllerClass('IndexController');
+        $this->assertMatchedRouteName('home');
+
+        // ViewModel template rendered within layout
+        $this->assertQuery('.container .jumbotron');
     }
 
     public function testInvalidRouteDoesNotCrash()
     {
-        $this->routeMatch->setParam('action', 'invalid-action');
-        $result = $this->controller->dispatch($this->request);
-        $response = $this->controller->getResponse();
-        $this->assertEquals(404, $response->getStatusCode());
+        $this->dispatch('/invalid/route', 'GET');
+        $this->assertResponseStatusCode(404);
     }
 }
