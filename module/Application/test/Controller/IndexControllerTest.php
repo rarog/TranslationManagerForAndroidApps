35654df10306ea7d4b60121385ca1976ee89a9b0
rarog <rarogit@gmail.com>
2018-05-01 22:50:29 +0200

    Refactoring test so it always simulates completed setup.

--- module/Application/test/Controller/IndexControllerTest.php
+++ module/Application/test/Controller/IndexControllerTest.php
@@ -16,15 +16,59 @@ namespace ApplicationTest\Controller;
 
 use Application\Module;
 use Application\Controller\IndexController;
+use Setup\Helper\DatabaseHelper;
+use Zend\ServiceManager\ServiceManager;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 use ZfcRbac\Guard\RoutePermissionsGuard;
 use ZfcRbac\Service\AuthorizationService;
 use ReflectionClass;
+use Application\View\Strategy\SetupAwareRedirectStrategy;
 
 class IndexControllerTest extends AbstractHttpControllerTestCase
 {
     private $authorizationService;
+
+    private function configureServiceManager(ServiceManager $services)
+    {
+        $authorizationService = $this->prophesize(AuthorizationService::class);
+        $this->authorizationService = $authorizationService;
+
+        $guards = $services->get('ZfcRbac\Guards');
+        foreach ($guards as $guard) {
+            if ($guard instanceof RoutePermissionsGuard) {
+                $reflection = new ReflectionClass(RoutePermissionsGuard::class);
+                $authorizationServiceProperty = $reflection->getProperty('authorizationService');
+                $authorizationServiceProperty->setAccessible(true);
+                $authorizationServiceProperty->setValue($guard, $authorizationService->reveal());
+            }
+        }
+
+        $databaseHelper = $this->prophesize(DatabaseHelper::class);
+        // Always simulating, that setup is complete.
+        $databaseHelper->isSetupComplete()->willReturn(true);
+
+        $setupAwareRedirectStrategy = $services->get(SetupAwareRedirectStrategy::class);
+
+        $reflection = new ReflectionClass(SetupAwareRedirectStrategy::class);
+        $databaseHelperProperty = $reflection->getProperty('setupDatabaseHelper');
+        $databaseHelperProperty->setAccessible(true);
+        $databaseHelperProperty->setValue($setupAwareRedirectStrategy, $databaseHelper->reveal());
+
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(AuthorizationService::class, $authorizationService->reveal());
+        $services->setService(DatabaseHelper::class, $databaseHelper->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    private function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
 
     public function setUp()
     {
@@ -37,20 +81,7 @@ class IndexControllerTest extends AbstractHttpControllerTestCase
 
         parent::setUp();
 
-        $serviceManager = $this->getApplicationServiceLocator();
-
-        $authorizationService = $this->prophesize(AuthorizationService::class);
-        $this->authorizationService = $authorizationService;
-
-        $guards = $serviceManager->get('ZfcRbac\Guards');
-        foreach ($guards as $guard) {
-            if ($guard instanceof RoutePermissionsGuard) {
-                $reflection = new ReflectionClass(RoutePermissionsGuard::class);
-                $authorizationServiceProperty = $reflection->getProperty('authorizationService');
-                $authorizationServiceProperty->setAccessible(true);
-                $authorizationServiceProperty->setValue($guard, $authorizationService->reveal());
-            }
-        }
+        $this->configureServiceManager($this->getApplicationServiceLocator());
     }
 
     public function testAboutActionCantBeAccessedByNonUsers()
